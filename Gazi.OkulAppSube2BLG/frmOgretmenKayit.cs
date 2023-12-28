9209069ce827037e93db5ce77b35c2172b856ea1
PlutonLib <108417633+plutonlib@users.noreply.github.com>
2023-12-28 21:15:55 +0300

    Yeni dosyalar
    Okulda verilen ödev projesinin dosyaları.

---
+++ Gazi.OkulAppSube2BLG/frmOgretmenKayit.cs
@@ -0,0 +1,52 @@
+﻿using OkulApp.BLL;
+using OkulApp.MODEL;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Gazi.OkulAppSube2BLG
+{
+    public partial class frmOgretmenKayit : Form
+    {
+        public frmOgretmenKayit()
+        {
+            InitializeComponent();
+        }
+
+        private void btnKaydet_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+                var obl = new OgretmenBL();
+                bool sonuc = obl.OgretmenEkle(new Ogretmen { Ad = txtOgretmenAd.Text.Trim(), Soyad = txtOgretmenSoyad.Text.Trim(), TC = txtOgretmenTc.Text.Trim() });
+                MessageBox.Show(sonuc ? "Ekleme başarılı!" : "Ekleme başarısız!!");
+            }
+            catch (SqlException ex)
+            {
+                switch (ex.Number)
+                {
+                    case 2627:
+                        MessageBox.Show("Bu numara daha önce kayıtlı");
+                        break;
+                    default:
+                        MessageBox.Show("Veritabanı Hatası!" + ex.Message);
+                        break;
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Bir hata oluştu!!");
+            }
+        }
+    }
+}
+    
+
---
+++ OkulApp.BLL/OgrenciBL.cs
@@ -0,0 +1,40 @@
+﻿using DAL;
+using OkulApp.MODEL;
+using System;
+using System.Data.SqlClient;
+using System.Runtime.CompilerServices;
+
+namespace OkulApp.BLL//Bussiness Logic Layer
+{
+    public class OgrenciBL
+    {
+        public bool OgrenciEkle(Ogrenci ogr)
+        {
+            try
+            {
+                SqlParameter[] p = {
+                                  new SqlParameter("@OgrenciAd",ogr.Ad),
+                                  new SqlParameter("@OgrenciSoyad",ogr.Soyad),
+                                  new SqlParameter("@OgrenciNo",ogr.Numara)
+                            };
+
+                var hlp = new Helper();
+                return hlp.ExecuteNonQuery("Insert into Ogrenciler  values (@OgrenciAd,@OgrenciSoyad,@OgrenciNo)", p) > 0;
+            }
+            catch (SqlException ex )
+            {
+                throw;
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+            finally
+            {
+               
+            }
+        }
+
+
+    }
+}
